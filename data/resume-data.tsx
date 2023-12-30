e1d51119ce781dab5617d0e63433b948a79f0a71
Rohianon <rohianon48@gmail.com>
2023-12-30 14:30:40 +0300

    add introductory pieces

---
+++ data/resume-data.tsx
@@ -0,0 +1,33 @@
+import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+
+export const RESUME_DATA = {
+  name: "Rohi Anon Ogula",
+  Initials: "RAO",
+  location: "Nairobi, Kenya",
+  locationLink: "https://www.google.com/maps/place/Nairobi,+Kenya/",
+  about: "An apt, optimistic, and enthusiastic Technologist aiming at positive development with Artificial Intelligence (AI) and Machine Learning (ML) (Natural Language Processing Enthusiast) without an accidental downfall of society",
+  summary: "Rohi is a Junior Data Scientist at Dalberg Data Insights with his most recent project revolving around building web dashboards that deliver insights to health professionals in Guinea, Madagascar, and Comoros. His interests revolve around building data and machine learning products with a lean toward public health, agriculture, and climate sectors.",
+  avatarUrl: "https://avatars.githubusercontent.com/u/41194018?v=4",
+  personalWebsite: "https://rohi.xyz",
+  contact: {
+    email: "rohianon48@gmail.com",
+    tel: "+254799031228",
+    social: [
+      {
+        name: "Github",
+        url: "https://www.github.com/Rohianon",
+        icon: GitHubIcon, 
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/rohi-anon/",
+        icon: LinkedInIcon,
+      },
+      {
+        name: "X",
+        url: "https://twitter.com/anon_rohi",
+        icon: XIcon,
+      }
+    ]
+  }
+}
